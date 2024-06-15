c6059c5bd56f2b7f72faa8d76e095e46a1e576bc
sarkersajib <csesajib1@gmail.com>
2024-06-15 11:23:06 +0600

    Module 1 Codes

---
+++ project_1/first_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def courses(request):
+    return HttpResponse("This is course page")
+
+    def about(request):
+    return HttpResponse("This is about page")
---
+++ project_1/project_1/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("This is Home page")
+
+def contact(request):
+    return HttpResponse("This is contact page")
