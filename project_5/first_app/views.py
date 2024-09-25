713e83eb9bd932c11c52b7634a949cb09375ade9
sarkersajib <csesajib1@gmail.com>
2024-09-25 14:53:06 +0600

    project_6 added

---
+++ project_5/first_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'./first_app/home.html')
+
+def about(request):
+    return render(request,'./first_app/about.html')
+
+def submit_form(request):
+    return render(request, './first_app/form.html')
