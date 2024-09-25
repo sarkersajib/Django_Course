a605873d4434d659be17faa99923d10a0c6ddd9a
sarkersajib <csesajib1@gmail.com>
2024-09-25 17:15:45 +0600

    project_7

---
+++ project_7/first_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class StudentModel(models.Model):
+    roll = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    father_name = models.CharField(max_length=30)
+    Address = models.TextField()
+
+    def __str__(self):
+        return f"Name :{self.name}"
